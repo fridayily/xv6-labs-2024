df721b286e1949040fe4317817401cc768456292
fridayily <zy@DESKTOP-AO0TA8E>
2024-10-07 23:50:18 +0800

    add commnent of fork

--- user/ex6.c
+++ user/ex6.c
@@ -1,24 +1,30 @@
-
 #include "kernel/types.h"
 #include "user/user.h"
+#include "kernel/fcntl.h"
 
-// ex5.c: fork then exec
+// ex6.c: run a command with output redirected
 
 int
 main()
 {
-  int pid, status;
+  int pid;
 
   pid = fork();
   if(pid == 0){
-    char *argv[] = { "echo", "THIS", "IS", "ECHO", 0 };
+    // 子进程
+    close(1);
+    open("out", O_WRONLY | O_CREATE | O_TRUNC);
+
+    char *argv[] = { "echo", "this", "is", "redirected", "echo", 0 };
     exec("echo", argv);
     printf("exec failed!\n");
     exit(1);
   } else {
-    printf("parent waiting\n");
-    wait(&status);
-    printf("the child exited with status %d\n", status);
+    // 等待子进程结束
+    // 子进程结束后 wait 返回子进程的 PID，并通过 status 参数传递子进程的退出状态 
+    // 当传递 (int *) 0 时，实际上传递的是 NULL 指针
+    // 如果传递 NULL 指针,父进程不关心子进程的退出状态
+    wait((int *) 0);
   }
 
   exit(0);
